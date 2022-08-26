9a0a1ec22efb849242742087b82c90419b7da9a6
PortilizenHub <matthewallanvincent09@gmail.com>
2022-08-25 19:25:22 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Password Generator
+
+This is the github repo for a Password Generator I made after a failed attempt at making a game for my portilizen jam (2022)
+
+so yeah, I had made a 12 letter password generator, nothin special
+
+# Important notes
+* I do not have access to your password
+* R to get another password
+* You can modify the code since its here on github
---
+++ source/PlayState.hx
@@ -0,0 +1,126 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxState;
+import flixel.text.FlxText;
+
+using StringTools;
+
+class PlayState extends FlxState
+{
+
+	var pass:FlxText;
+
+	var passwordThing:String;
+
+	var letterData:Array<String> = [
+		'a',
+		'b',
+		'c',
+		'd',
+		'e',
+		'f',
+		'g',
+		'h',
+		'i',
+		'j',
+		'k',
+		'l',
+		'm',
+		'n',
+		'o',
+		'p',
+		'q',
+		'r',
+		's',
+		't',
+		'u',
+		'v',
+		'w',
+		'x',
+		'y',
+		'z',
+		'0',
+		'9',
+		'8',
+		'7',
+		'6',
+		'5',
+		'4',
+		'3',
+		'2',
+		'1',
+		'`',
+		'!',
+		'@',
+		'#', 
+		'$',
+		'%',
+		'^',
+		'&',
+		'*', 
+		'(', 
+		')',
+		'_', 
+		'+', 
+		'{',
+		'}', 
+		'|', 
+		':',
+		'"',
+		'<',
+		'>',
+		'?',
+		'.',
+		',',
+		"'",
+		';',
+		'[',
+		']',
+		'=',
+		'-',
+		'`'
+		
+	];
+
+	var passwordsGenerated:Int = 1;
+
+	override public function create()
+	{
+		trace('password NO.' + passwordsGenerated);
+
+		passwordThing = passwordString();
+
+		pass = new FlxText(180, 60, 0, passwordThing, 24);
+		add(pass);
+
+		super.create();
+	}
+
+	override public function update(elapsed:Float)
+	{
+
+		if (FlxG.keys.justReleased.R)
+		{
+			passwordsGenerated++;
+
+			trace('password NO.' + passwordsGenerated);
+			
+			passwordThing = passwordString();
+
+			pass.text = passwordThing;
+		}
+
+		super.update(elapsed);
+	}
+
+	function passwordString()
+	{
+		var password:String = letterData[FlxG.random.int(0, letterData.length)] + letterData[FlxG.random.int(0, letterData.length)] + letterData[FlxG.random.int(0, letterData.length)]
+			+ letterData[FlxG.random.int(0, letterData.length)] + letterData[FlxG.random.int(0, letterData.length)] + letterData[FlxG.random.int(0, letterData.length)]
+			+ letterData[FlxG.random.int(0, letterData.length)] + letterData[FlxG.random.int(0, letterData.length)] + letterData[FlxG.random.int(0, letterData.length)]
+			+ letterData[FlxG.random.int(0, letterData.length)] + letterData[FlxG.random.int(0, letterData.length)] + letterData[FlxG.random.int(0, letterData.length)];
+
+		return password;
+	}
+}
